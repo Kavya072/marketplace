725eb7e799763814d66fc976d518ceb499bcd7a5
Aditya Singh <greatadityasingh@gmail.com>
2024-07-12 02:04:07 +0530

    000 | Aditya | update db and service

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -25,19 +25,10 @@ export class UserServiceService {
   // updateuser(id:any,inputdata:any){
   //   return this.http.put(this.apiurl+'/'+id,inputdata);
   // }
-  // getuserrole(){
-  //   return this.http.get('http://localhost:3000/role');
-  // }
   isloggedin(){
     return sessionStorage.getItem('username')!=null;
   }
-  // getrole(){
-  //   return sessionStorage.getItem('role')!=null?sessionStorage.getItem('role')?.toString():'';
-  // }
   GetAllProduct(){
     return this.http.get(this.apiurl+'registeredProductList');
   }
-  // Getaccessbyrole(role:any,menu:any){
-  //   return this.http.get('http://localhost:3000/roleaccess?role='+role+'&menu='+menu)
-  // }
 }
